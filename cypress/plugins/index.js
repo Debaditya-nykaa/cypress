9d6a32b7a288acb8b484207793a2519154e1238e
anurag <anuraggambhir88@gmail.com>
2020-01-12 15:41:03 +0530

    Changes in master

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -11,7 +11,24 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-module.exports = (on, config) => {
+//module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+//}
+
+const fs = require('fs-extra')
+const path = require('path')
+
+function getConfigurationByFile (file) {
+  const pathToConfigFile = path.resolve('.', 'config', `${file}.json`)
+
+  return fs.readJson(pathToConfigFile)
+}
+
+// plugins file
+module.exports = (on, config) => {
+  // accept a configFile value or use development by default
+  const file = config.env.configFile || 'development'
+
+  return getConfigurationByFile(file)
 }
